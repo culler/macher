fdd1fc338c039bac0d3829845e33bdb91a5880ec
Marc Culler <culler@users.noreply.github.com>
2021-02-14 15:58:25 -0600

    Bump version minor number.

--- macher.c
+++ macher.c
@@ -8,7 +8,7 @@
  * See the file License.txt included with the source code distribution.
  *
  */
-#define MACHER_VERSION "1.2"
+#define MACHER_VERSION "1.3"
 
 #include <stdio.h>
 #include <stdlib.h>
